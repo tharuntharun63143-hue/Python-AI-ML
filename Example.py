11a2a02483682894404832c62d155a3a0cb9ef79
Tharun <tharuntharun63143@gmail.com>
2026-02-04 21:23:42 +0530

    Add examples of NumPy array operations Example

---
+++ Example.py
@@ -0,0 +1,20 @@
+#example-1
+arr = np.array([[1,3,5,7,9],
+                [11,13,15,17,19],
+                [21,23,25,27,29],
+                [12,13,14,25,16],
+                [8,6,4,2,1]])
+arr1 = np.array([[1,2,3],
+                 [4,6,7],
+                 [9,4,5]])
+print("array middle result",arr[2:5])
+
+example-2
+arr = np.array([2,4,6,8,10,12,14,16,18,20])
+print(" array even number:",arr-1)
+
+#exmaple-3
+a = np.array([1,2,3,4])
+b = np.array([1,2,3,4])
+print("broadcasting result", a+b)
+print("broadcasting result", a*b)
