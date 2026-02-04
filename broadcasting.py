3b0e6a95e77e7f6b4c82f9eddd5bd9f1af3762ef
Tharun <tharuntharun63143@gmail.com>
2026-02-04 21:18:29 +0530

    Add broadcasting example in broadcasting.py

---
+++ broadcasting.py
@@ -0,0 +1,8 @@
+#broadcasting method
+a = np.array([[1,2,3,],
+              [4,5,6],
+              [7,8,9]])
+b = np.array([10,20,30])
+print("broadcasting result:",a + b)
+print("broadcasting result:",a * b)
+print("broadcasting result:",a - b)
