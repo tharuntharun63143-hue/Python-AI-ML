d5cd1f8cb5e0989470294a1eb6fb7c46c62d7bc3
Tharun <tharuntharun63143@gmail.com>
2026-02-04 21:13:13 +0530

    Create methods of array.py

---
+++ methods of array.py
@@ -0,0 +1,23 @@
+# array method in Numpy
+
+import numpy as np
+
+#list of array
+arr1 = np.array([1,2,3])
+print("from arr1:",arr1)
+
+#Zeros onces method
+zeros = np.zeros(0,7,3)
+onces = np.onces(1,2,3)
+print("Zeros:",zero)
+print("onces:",once)
+
+#arange method
+range_arr = np.arange(0.3,9)
+print("arange:",range_arr)
+
+#linspace method
+lin_arr = np.linspace( 0,3,9 )
+print("linspace:",lin_arr)
+#%%
+
