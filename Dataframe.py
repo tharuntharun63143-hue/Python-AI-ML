c68d2416939fe563dc9b57f44701828a5bfb7680
Tharun <tharuntharun63143@gmail.com>
2026-02-04 21:27:40 +0530

    Add initial DataFrame implementation with sample data

---
+++ Dataframe.py
@@ -0,0 +1,8 @@
+# dataframe in 2d data and operation
+data = {
+    'name':["raju","Ramu","ravi","ramya","rukmini"],
+    'age':[24,22,21,20,18],
+    'marks':[90,86,78,87,99]
+}
+df = pd.DataFrame(data)
+print("list of data\n",df)
